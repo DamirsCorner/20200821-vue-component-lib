28d2e783566438e6cf7566a4337fc994c5b0de50
Damir Arh <damir.arh@gmail.com>
2020-07-18 12:43:51 +0200

    Create component library

---
+++ vue-component/src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button type="button" @click="click()">
+    <slot>{{ text }}</slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Button extends Vue {
+  @Prop({ type: String, required: true }) readonly text!: string;
+
+  click() {
+    this.$emit("click");
+  }
+}
+</script>
